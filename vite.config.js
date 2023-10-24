9c07618bdcdcbba50b3a15f9ab7449fd4030792f
shawn edgell <shawn_edgell@hotmail.com>
2023-10-23 22:35:18 -0400

    fixed build config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Memory-Card/',
+  base: '/Memory-Card-2/',
   plugins: [react()],
 })
